adc320d6f738c1b212d0b4fe99e1d6cd145b5903
magnus <magnuspicataggio@gmail.com>
2026-06-09 22:39:18 -0400

    Added the boundingSphere along with intersectData classes. The IntersectData class will tell us how deep the bounding sphere has intersected along with providing whether they have intersected or not.

---
+++ src/physics2/intersectData.cpp
@@ -0,0 +1,4 @@
+#include "intersectData.h"
+
+bool IntersectData::getDoesIntersect() const { return m_doesIntersect; }
+bool IntersectData::getDistance() const { return distance; }
---
+++ src/physics2/intersectData.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class IntersectData {
+   private:
+    const bool m_doesIntersect;
+    const float distance;
+
+   public:
+    IntersectData(const bool doesIntersect, const float distance);
+
+    bool getDoesIntersect() const;
+    bool getDistance() const;
+};
