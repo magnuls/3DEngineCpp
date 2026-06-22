0c23dcbf746ae78771a057e0cde4d90a20899230
magnus <magnuspicataggio@gmail.com>
2026-06-22 04:11:23 -0400

    finished unit testing AABB collisions and distances

--- src/physics2/AABB.h
+++ src/physics2/AABB.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "../core/math3d.cpp"
+#include "../core/math3d.h"
 #include "intersectData.h"
 
 // Axis Algined bounding box
@@ -7,8 +7,8 @@
 namespace Physics {
 class AABB {
    private:
-    const Vector3f min;
-    const Vector3f max;
+    Vector3f min;
+    Vector3f max;
 
    public:
     AABB(const Vector3f& bottom_left, const Vector3f& top_right);
