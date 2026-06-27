256a6e66172898e681682d09d620f207cc9b3a7c
magnus <magnuspicataggio@gmail.com>
2026-06-27 02:23:16 -0400

    Added unit tests for Plane vs all Colliders

--- src/physics2/collisionDispatch.cpp
+++ src/physics2/collisionDispatch.cpp
@@ -6,6 +6,7 @@
 #include "AABB.h"
 #include "boundingSphere.h"
 #include "intersectData.h"
+#include "plane.h"
 
 using namespace Physics;
 
@@ -56,4 +57,55 @@ template <>
 IntersectData Physics::collision<BoundingSphere, AABB>(const BoundingSphere& B,
                                                        const AABB& A) {
     return Physics::collision(A, B);
+}
+
+template <>
+IntersectData Physics::collision<BoundingSphere, Plane>(const BoundingSphere& B,
+                                                        const Plane& P) {
+    Plane normalized{P.normalize()};
+    float dist{std::abs(normalized.getNorm().Dot(B.getCenter()) -
+                        normalized.getScaler())};
+
+    IntersectData inter(dist <= B.getRadius(), dist);
+    return inter;
+}
+
+template <>
+IntersectData Physics::collision<Plane, BoundingSphere>(
+    const Plane& P, const BoundingSphere& B) {
+    return collision(B, P);
+}
+
+template <>
+IntersectData Physics::collision<Plane, Plane>(const Plane& P1,
+                                               const Plane& P2) {
+    Plane n1{P1.normalize()}, n2{P2.normalize()};
+    IntersectData inter(true, 0);
+    if (n1.getNorm().Cross(n2.getNorm()).Length() < 1e-6f) {
+        inter.m_doesIntersect = false;
+        inter.distance = std::abs(n1.getScaler() - n2.getScaler());
+    }
+    return inter;
+}
+
+template <>
+IntersectData Physics::collision<Plane, AABB>(const Plane& P, const AABB& A) {
+    // Normalized norm lmao
+    Plane normalized{P.normalize()};
+    Vector3f norm_norm{normalized.getNorm()};
+    Vector3f A_center{(A.getMax() + A.getMin()) / 2};
+    float radius{((A.getMax().GetX() - A.getMin().GetX()) / 2) *
+                     std::abs(norm_norm.GetX()) +
+                 ((A.getMax().GetY() - A.getMin().GetY()) / 2) *
+                     std::abs(norm_norm.GetY()) +
+                 ((A.getMax().GetZ() - A.getMin().GetZ()) / 2) *
+                     std::abs(norm_norm.GetZ())};
+    float distance{std::abs(norm_norm.Dot(A_center) - normalized.getScaler())};
+    IntersectData inter(distance <= radius, distance);
+    return inter;
+}
+
+template <>
+IntersectData Physics::collision<AABB, Plane>(const AABB& A, const Plane& P) {
+    return collision(P, A);
 }
